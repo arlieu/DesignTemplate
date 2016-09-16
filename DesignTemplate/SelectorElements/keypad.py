8a32b818c92f0dd83de7352f1f4fd7be1758dbaf
arlieu <avelieu@gmail.com>
2016-09-16 14:39:56 -0400

    Virtual keyboard and textbox implemented. Handles real keyboard input. Fix backspace and enter to be compatible with both virtual and real keys.

--- DesignTemplate/SelectorElements/keypad.py
+++ DesignTemplate/SelectorElements/keypad.py
@@ -11,39 +11,65 @@ class KeyPad(GridLayout):
         self.spacing = 10
         self.textInput = list('')
         self.index = 0
-        self.createButtons()
+        self.capsPressed = False
+        self.shiftPressed = False
+        self.createButtons(False, False)
 
-    def createButtons(self):
-        bList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', \
-            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', \
-            '', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter', \
-            '', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift', 'CLEAR']
+    def createButtons(self, shift, caps):
+        self.clear_widgets()
+        if shift == True:
+           bList = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '<--', \
+                'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', \
+                'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'enter', \
+                'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'shift', '']
+        elif caps == True:
+            bList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '<--', \
+                'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\\', \
+                'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', "'", 'enter', \
+                'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'shift', '']
+        else:
+             bList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '<--', \
+                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', \
+                'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter', \
+                'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift', '']
+
+
         for i in bList:
-            if i == '':
-                self.add_widget(Label(text=''))
+            if len(i) > 1:
+                self.add_widget(Button(text=str(i), font_size = 12, on_release=self.btnPress))
             else:
-                if len(i) > 1:
-                    self.add_widget(Button(text=str(i), font_size= min(self.height, self.width)/10, on_release=self.btnPress))
-                else:
-                    self.add_widget(Button(text=str(i), on_release=self.btnPress))
+                self.add_widget(Button(text=str(i), on_release=self.btnPress))
 
     def btnPress(self, btn):
         self.index += 1
 
-        if btn.text == 'backspace':
+        if btn.text == '<--':
             if len(self.textInput) > 0:
                 self.textInput.pop()
                 App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text = ''
                 for i in range(0, len(self.textInput)):
                     App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text += self.textInput[i]
+        elif btn.text == 'caps':
+            self.shiftPressed = False
+            if (self.capsPressed == False):
+                self.capsPressed = True
+                self.createButtons(False, True) 
+            else:
+                self.capsPressed = False
+                self.createButtons(False, False)
         elif btn.text == 'enter':
             self.textInput.append('\n')
             App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text += '\n'
         elif btn.text == 'shift':
-            pass 
-        elif btn.text.lower() == 'clear':
-            self.textInput = []
-            App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text = ''
+            self.capsPressed = False
+            if (self.shiftPressed == False):
+                self.shiftPressed = True
+                self.createButtons(True, False) 
+            else:
+                self.shiftPressed = False
+                self.createButtons(False, False)
+        elif btn.text == '':
+            App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text += ' '
         else:
             self.textInput.append(btn.text)
             App.get_running_app().root.ids.selectorScreen.ids.keypadScreen.ids.textBox.text += btn.text
